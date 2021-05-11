084dd52dba2d6d319acefaf73d3c00d1ffee8796
wilmer-ibarra7 <81928029+wilmer-ibarra7@users.noreply.github.com>
2021-05-10 19:48:41 -0700

    Add files via upload

---
+++ OldCoffeeMachine.java
@@ -0,0 +1,13 @@
+package CoffeeMachineInterface;
+
+public class OldCoffeeMachine {
+	
+	public void selectFlavor1() {
+		System.out.println(" Flavor1 selected");
+	}
+
+	public void selectFlavor2() {
+		System.out.println(" Flavor2 selected");
+	}
+
+ }
